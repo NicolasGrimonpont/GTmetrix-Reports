08d2767b3efafd6ba9218536100e4154d8014ba9
Nico G <grimnicolas@hotmail.fr>
2021-02-12 18:44:59 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Gtmetrix;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Gtmetrix::class, 'index']);
