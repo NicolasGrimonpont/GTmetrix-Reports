b96380eeb24ce622e1c171cb75649f8dc71ba527
Nico G <grimnicolas@hotmail.fr>
2021-02-16 19:43:41 -0300

    update datatables third party

---
+++ resources/plugin/js/vendors/datatables.js
@@ -0,0 +1,52 @@
+/**
+ * Load required plugin.
+ */
+
+// Reference:
+// https://datatables.net/download/
+
+
+// require('pdfmake'); // Export to PDF
+// require('datatables.net'); // Core
+// require('datatables.net-bs4'); // Bootstrasp 4 Core
+// require('datatables.net-buttons'); // Buttons extension
+require('datatables.net-buttons-bs4')(); // Buttons extension for Bootstrap 4
+require('datatables.net-buttons/js/buttons.colVis.js')(); // Column visibility
+require('datatables.net-buttons/js/buttons.html5.js')();  // HTML 5 file export
+// require('datatables.net-buttons/js/buttons.flash.js')();
+require('datatables.net-buttons/js/buttons.print.js')();  // Print view button
+// require('datatables.net-responsive-bs4')();
+
+
+/**
+ * Configure the plugin.
+ */
+
++ function ($) {
+    page.registerVendor('Datatables');
+
+    page.initDatatables = function () {
+        $('[data-provide~="datatables"]').each(function () {
+
+            // Custom with some options
+            var table = $(this).DataTable({
+                dom: 'Bfrltip',
+                buttons: [
+                    {
+                        extend: 'colvis',
+                        text: 'Show Columns'
+                    },
+                    'copy', 'csv', 'print'
+                ],
+                columnDefs: [
+                    {
+                        "targets": [0, 2, 3, 4, 7, 9, 11, 12, 13, 14, 15, 16, 17, 18, 20, 22, 24, 25, 26, 27, 28, 29, 30],
+                        "visible": false
+                    }
+                ],
+                lengthMenu: [[10, 25, 50, 100, 250, 500, -1], [10, 25, 50, 100, 250, 500, "All"]]
+            });
+
+        });
+    }
+}(jQuery);
