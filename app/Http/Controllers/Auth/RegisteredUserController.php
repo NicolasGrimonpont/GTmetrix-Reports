209f30a16716dec3c5ed641d20ca473051774389
Nico G <grimnicolas@hotmail.fr>
2021-02-18 02:48:43 -0300

    add custom message for filtering email address

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,6 +42,11 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'custom' => [
+                'email' => [
+                    'ends_with:tavanoteam.com' => 'Your email have to be provided by Tavanoteam.'
+                ],
+            ],
         ]));
 
         event(new Registered($user));
