c03f24379d62225169d03d6b1d92a80d5e64f5b5
Nico G <grimnicolas@hotmail.fr>
2021-03-15 10:55:45 -0300

    set up cron task daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('App\Http\Controllers\Cron@schedule')->everyMinute();
+        $schedule->call('App\Http\Controllers\Cron@schedule')->daily();
     }
 
     /**
